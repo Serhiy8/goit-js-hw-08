07fe42d7c8c44556c6212e259680acffdfff2889
Serhiy8 <markovsm86@gmail.com>
2023-06-27 16:49:04 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const player = new Player('vimeo-player');
+
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+player.on('timeupdate', throttle(onPlay, 1000));
